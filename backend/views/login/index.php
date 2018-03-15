3050943f3d57cf8d783eeba0471f6be8506fff5f
admin <809829406@qq.com>
2018-03-15 16:51:42 +0800

    1

--- backend/views/login/index.php
+++ backend/views/login/index.php
@@ -11,7 +11,7 @@ echo $form->field($model,'remember')->checkboxList(['1'=>'记住密码']);
 //验证码
 echo $form->field($model,'verifyCode')->widget(\yii\captcha\Captcha::className(),[
     'captchaAction'=>'login/captcha',
-    'template'=>'<div class="row"><div class="col-xs-2">{input}</div><div class="col-xs-1">{image}</div></div> '
+    'template'=>'<div class="row"><div class="col-sm-4">{input}</div><div class="col-sm-4">{image}</div></div> '
 ]);
 
 echo '<button type="submit" class="btn btn-primary">登录</button>';
